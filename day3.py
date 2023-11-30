80b9f3b486e6849b4315a8c97b66412a0c11ca21
kylychbekEraliev <kylychbekeraliev2000@gmail.com>
2023-11-30 21:09:46 +0600

    growth hungry challenge 3day

---
+++ day3.py
@@ -0,0 +1,19 @@
+total=0
+for i in range (1,5):
+    total +=i
+print(total)
+
+tatal2=0
+j=1
+while j<5:
+    tatal2 +=j
+    j +=1
+print(tatal2)
+
+given_list=[5,4,4,3,1,-1,-2,-3,-5]
+total3=0
+i = 0
+while given_list[i]>0:
+    total3 += given_list[i]
+    i +=1
+print(total3)
