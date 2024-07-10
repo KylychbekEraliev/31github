a72e661c3fc7a5b8198bb4ff7a0bd652802f0081
Kylychbek Zhumabai uulu <100015132+KylychbekEraliev@users.noreply.github.com>
2024-07-10 22:52:30 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,33 +1,11 @@
-db.studentInfo.insert({
-  name: { firstName: "John", lastName: "Doe" },
-  class: 6,
-  rollNo: 23,
-  subjects: ["Maths", "Physics", "English", "Chemistry"],
-  attendance: {
-    January: "90%",
-    February: "85%",
-    March: "98%"
-  }
-});
+// Update script.js
 
-db.studentInfo.insert({
-  name: { firstName: "Peter", lastName: "A" },
-  rollNo: 24,
-  subjects: ["Maths", "Physics", "English", "Chemistry"],
-  attendance: {
-    January: "97%",
-    February: "99%",
-    March: "100%"
-  }
-});
-
-db.studentInfo.insert({
-  name: { firstName: "Dave", lastName: "J" },
-  rollNo: 27,
-  subjects: ["Maths", "Physics", "English", "Chemistry"],
-  attendance: {
-    January: "87%",
-    February: "99%",
-    March: "100%"
-  }
-});
+// MongoDB update operation
+db.studentInfo.updateMany(
+    {},
+    {
+        $set: {
+            'attendance.November': '$unset: { "attendance.January": "" }'
+        }
+    }
+);
